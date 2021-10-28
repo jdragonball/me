441fc3f7f208c7d99e781ea08d7e3ed350f0fff8
jdragonball <jaeyong.sinn@gmail.com>
2021-10-28 21:13:42 +0900

    me

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
-import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.133.1-a8rkd0QTHl2tMZXZJAEw/mode=imports,min/optimized/three.js';
-import { FirstPersonControls } from "https://cdn.skypack.dev/pin/three@v0.133.1-a8rkd0QTHl2tMZXZJAEw/mode=imports,min/unoptimized/examples/jsm/controls/FirstPersonControls.js";
+import * as THREE from "https://cdn.skypack.dev/pin/three@v0.133.1-a8rkd0QTHl2tMZXZJAEw/mode=imports,min/optimized/three.js";
+
+const keyStates = {};
+
+const STEPS_PER_FRAME = 5;
+
+let mouseTime = 0;
 
 function main() {
   const canvas = document.querySelector("#c");
@@ -13,9 +18,9 @@ function main() {
     0.1,
     1000
   );
-  camera.position.set(0, 30, 100);
+  camera.position.set(0, 20, 100);
   camera.lookAt(0, 0, 0);
-  camera.rotation.order = 'YXZ';
+  camera.rotation.order = "YXZ";
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color("white");
@@ -25,20 +30,32 @@ function main() {
   var axes = new THREE.AxesHelper(1000);
   scene.add(axes);
 
-  // const camControls = new FirstPersonControls(camera);
-  // camControls.lookSpeed = 0.2;
-  // camControls.movementSpeed = 20;
-  // camControls.autoForward = false;
-  // camControls.noFly = true;
-  // camControls.lookVertical = true;
-  // camControls.constrainVertical = true;
-  // camControls.verticalMin = 0.0;
-  // camControls.verticalMax = 2.0;
-  // camControls.lookAt(new THREE.Vector3(0, -100, 0));
-  // camControls.heightMax = 0.1;
-  // camControls.heightMin = 0.1;
-  // camControls.lon = -150;
-  // camControls.lat = 10;
+  const playerVelocity = new THREE.Vector3();
+  const playerDirection = new THREE.Vector3();
+
+  document.addEventListener("keydown", (event) => {
+    keyStates[event.code] = true;
+  });
+  
+  document.addEventListener("keyup", (event) => {
+    keyStates[event.code] = false;
+  });
+  
+  document.addEventListener("mousedown", () => {
+    document.body.requestPointerLock();
+  
+    mouseTime = performance.now();
+  });
+  
+  document.addEventListener("mouseup", () => {
+  });
+  
+  document.body.addEventListener("mousemove", (event) => {
+    if (document.pointerLockElement === document.body) {
+      camera.rotation.y -= event.movementX / 500;
+      camera.rotation.x -= event.movementY / 500;
+    }
+  });
 
   // 평면
   {
@@ -46,12 +63,12 @@ function main() {
     const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.receiveShadow = true;
-  
+
     plane.rotation.x = -0.5 * Math.PI;
     plane.position.x = 0;
     plane.position.y = 0;
     plane.position.z = 0;
-  
+
     scene.add(plane);
   }
 
@@ -59,11 +76,60 @@ function main() {
   {
     const ambientLight = new THREE.AmbientLight(0x0c0c0c);
     scene.add(ambientLight);
-  
+
     const spotLight = new THREE.SpotLight(0xffffff);
     spotLight.position.set(-40, 60, -10);
     spotLight.castShadow = true;
     scene.add(spotLight);
+  }
+
+  function getForwardVector() {
+    camera.getWorldDirection(playerDirection);
+    playerDirection.y = 0;
+    playerDirection.normalize();
+
+    return playerDirection;
+  }
+
+  function getSideVector() {
+    camera.getWorldDirection(playerDirection);
+    playerDirection.y = 0;
+    playerDirection.normalize();
+    playerDirection.cross(camera.up);
+
+    return playerDirection;
+  }
+
+  function controls(deltaTime) {
+    // gives a bit of air control
+    const speedDelta = deltaTime * 8;
+
+    if (keyStates["KeyW"]) {
+      playerVelocity.add(getForwardVector().multiplyScalar(speedDelta));
+    }
+
+    if (keyStates["KeyS"]) {
+      playerVelocity.add(getForwardVector().multiplyScalar(-speedDelta));
+    }
+
+    if (keyStates["KeyA"]) {
+      playerVelocity.add(getSideVector().multiplyScalar(-speedDelta));
+    }
+
+    if (keyStates["KeyD"]) {
+      playerVelocity.add(getSideVector().multiplyScalar(speedDelta));
+    }
+  }
+
+  function updatePlayer(deltaTime) {
+    let damping = Math.exp(-4 * deltaTime) - 1;
+
+    playerVelocity.addScaledVector(playerVelocity, damping);
+
+    const deltaPosition = playerVelocity.clone().multiplyScalar(deltaTime);
+
+    camera.position.x += deltaPosition.x * 50;
+    camera.position.z += deltaPosition.z * 50;
   }
 
   function resizeRendererToDisplaySize(renderer) {
@@ -78,7 +144,7 @@ function main() {
   }
 
   function render() {
-    var delta = clock.getDelta();
+    const deltaTime = Math.min( 0.05, clock.getDelta() ) / STEPS_PER_FRAME;
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
@@ -86,10 +152,11 @@ function main() {
       camera.updateProjectionMatrix();
     }
 
-    // console.log(camera.getWorldDirection)
-    
-    // camControls.update(delta);
-    // renderer.clear();
+    for (let i = 0; i < STEPS_PER_FRAME; i++) {
+      controls(deltaTime);
+      updatePlayer(deltaTime);
+    }
+
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
