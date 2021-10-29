7beba698bab209a2feb0ce7880bcee365c951494
jdragonball <jaeyong.sinn@gmail.com>
2021-10-29 14:07:52 +0900

    me

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-// import { GLTFLoader } from "../jsm/loaders/GLTFLoader.js";
+import * as THREE from "./three/build/three.min.js";
+import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
 
 const keyStates = {};
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './index.js', // 의존성 그래프의 시작점!! -> 이 파일이 실행된거임. 
+  entry: "./index.js", // 의존성 그래프의 시작점!! -> 이 파일이 실행된거임.
   output: {
-    filename: 'main.js', // 번들링 결과물로써 dist 파일 내부에 생성되고, index.html에 import 됨.
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js", // 번들링 결과물로써 dist 파일 내부에 생성되고, index.html에 import 됨.
+    path: path.resolve(__dirname, "dist"),
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  mode: "production",
+  // devServer: {
+  //   contentBase: __dirname,
+  //   inline: true,
+  //   hot: true,
+  //   host: "localhost",
+  //   port: 5500,
+  // }
 };
