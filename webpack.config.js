c7d46fd01bfd0af3ec431dd372773dfaf287aff9
jdragonball <jaeyong.sinn@gmail.com>
2021-11-01 17:24:35 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,81 @@
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const webpackMode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  entry: "./index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
-  mode: "development",
-  devServer: {
-    static: {
-      directory: __dirname
-    },
-    // liveReload: true,
-    // hot: true,
-    open: true,
-  },
+	mode: webpackMode,
+	entry: {
+		main: './src/main.js',
+	},
+	output: {
+		path: path.resolve('./dist'),
+		filename: '[name].js'
+	},
+	// es5로 빌드 해야 할 경우 주석 제거
+	// 단, 이거 설정하면 webpack-dev-server 3번대 버전에서 live reloading 동작 안함
+	// target: ['web', 'es5'],
+	devServer: {
+		liveReload: true
+	},
+	optimization: {
+		minimizer: webpackMode === 'production' ? [
+			new TerserPlugin({
+				terserOptions: {
+					compress: {
+						drop_console: true
+					}
+				}
+			})
+		] : [],
+		splitChunks: {
+			chunks: 'all'
+		}
+	},
+	// externals: {
+		// 빌드 과정에서 제외하고 그대로 사용할 패키지 지정
+		// 예시 - foo라는 이름의 모듈이라면 아래처럼 입력
+		// foo: "foo"
+	// },
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.js$/,
+				enforce: 'pre',
+				use: ['source-map-loader'],
+			}
+		]
+	},
+	plugins: [
+		new webpack.BannerPlugin({
+			banner: `
+				Created by jdragonball
+			`
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			minify: process.env.NODE_ENV === 'production' ? {
+				collapseWhitespace: true,
+				removeComments: true,
+			} : false
+		}),
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: "./src/favicon.ico", to: "./favicon.ico" },
+				{ from: "./src/assets/", to: "./assets/" },
+				// { from: "./src/main.css", to: "./main.css" },
+				// { from: "./src/images", to: "./images" },
+			],
+		})
+	]
 };
