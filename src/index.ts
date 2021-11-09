1c410032d5628857459f5b70d584d070289f58bd
jdragonball <jaeyong.sinn@gmail.com>
2021-11-09 12:24:02 +0900

    build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { init } from "./init"; init();
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
