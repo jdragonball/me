bb6646445be40408b983ab7d1b744609d89a2572
jdragonball <jaeyong.sinn@gmail.com>
2021-11-09 12:20:25 +0900

    build

--- src/index.ts
+++ src/index.ts
@@ -74,8 +74,21 @@ const ThreeScene = () => {
     );
 
     document.addEventListener("keydown", (event) => {
+      if (event.code === "KeyF") {
+        toggleFullScreen();
+      }
       keyStates[event.code] = true;
     });
+
+    const toggleFullScreen = () => {
+      if (!document.fullscreenElement) {
+        document.body.requestFullscreen();
+      } else {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        }
+      }
+    }
 
     document.addEventListener("keyup", (event) => {
       keyStates[event.code] = false;
